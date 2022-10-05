d21e5f5251cf4d3365de6bf4a461f7a473d1303c
DeadBoyPiotrek <87661735+DeadBoyPiotrek@users.noreply.github.com>
2022-10-05 21:26:23 +0200

    hello

--- pages/api/getPrice.js
+++ pages/api/getPrice.js
@@ -1,8 +1,13 @@
 import { replacePhotos } from '../../mongodb/dbFunctions';
-import { getPrice } from '../../puppeteer/puppeteerFunctions';
+// import { getPrice } from '../../puppeteer/puppeteerFunctions';
 const handler = async (req, res) => {
-  const photos = await getPrice(req, res);
-  await replacePhotos(photos);
+  // const photos = await getPrice(req, res);
+  // console.log('photos', photos);
+  const photos2 = {
+    photo1dot2: 'lol',
+    photo2dot2: 'lol2',
+  };
+  await replacePhotos(photos2);
   res
     .status(200)
     .json({ name: ' I guess, puppeteer and mongodb worked somehow 😮' });
