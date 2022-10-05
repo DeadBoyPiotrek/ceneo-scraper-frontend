197471f80d2067eb0146cd7732d1944954f3d758
DeadBoyPiotrek <87661735+DeadBoyPiotrek@users.noreply.github.com>
2022-10-05 15:23:56 +0200

    add more lambda stuff

--- puppeteer/puppeteerFunctions.js
+++ puppeteer/puppeteerFunctions.js
@@ -1,6 +1,8 @@
+let chrome = {};
 let puppeteer;
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  chrome = require('chrome-aws-lambda');
   puppeteer = require('puppeteer-core');
 } else {
   puppeteer = require('puppeteer');
@@ -12,21 +14,51 @@ const url = 'https://www.ceneo.pl/';
 const item = 'rtx 3060';
 //! change this to something else 😵🐊
 
-//! puppeteer
 export const getPrice = async () => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    ignoreHTTPSErrors: true,
+  //* copied code
+
+  let options = {
     defaultViewport: {
       width: 1920,
       height: 1080,
     },
-  });
+  };
 
-  const page = await browser.newPage();
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: {
+        width: 1920,
+        height: 1080,
+      },
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
+  let browser = await puppeteer.launch(options);
+
+  let page = await browser.newPage();
+
   await page.setUserAgent(
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
   );
+  //* copied code
+
+  // const browser = await puppeteer.launch({
+  //   headless: true,
+  //   ignoreHTTPSErrors: true,
+  //   defaultViewport: {
+  //     width: 1920,
+  //     height: 1080,
+  //   },
+  // });
+
+  // const page = await browser.newPage();
+  // await page.setUserAgent(
+  //   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+  // );
   await page.goto(url);
   await page.focus('#form-head-search-q');
   await page.keyboard.type(item);
@@ -34,12 +66,9 @@ export const getPrice = async () => {
   await page.keyboard.press('Enter');
   await page.waitForSelector('.js_seoUrl');
   const photo1 = await page.screenshot({});
-  // .toString('base64');
   await page.click('.js_seoUrl');
   await page.waitForTimeout(500);
   const photo2 = await page.screenshot({});
-  // .toString('base64');
-
   const photo1dot2 = await photo1.toString('base64');
   const photo2dot2 = await photo2.toString('base64');
   // console.log(photo1, photo2, photo1dot2);
@@ -48,5 +77,3 @@ export const getPrice = async () => {
   await browser.close();
   return { photo1dot2, photo2dot2 };
 };
-
-//! puppeteer
